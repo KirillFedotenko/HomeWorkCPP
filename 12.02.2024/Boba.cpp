e771b89631fda6622f1e77a124c7e0dbbc3ac878
KirillFedotenko <159914000+KirillFedotenko@users.noreply.github.com>
2024-02-15 15:44:47 +0300

    Add files via upload

---
+++ 12.02.2024/Boba.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Zadanie 1:" << endl;
+	cout << " " << endl;
+	for (int i = 0; i < 15; i++) cout << "Around the world";
+	cout << "\t\t\t\tAroundTheWorld," << endl;
+	cout << "\t\t\t\tBy DaftPank" << endl; 
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+	cout << "Zadanie 2:" << endl;
+	cout << "Every" << endl;
+	cout << "\tHunter" << endl;
+	cout << "\t\tWants" << endl;
+	cout << "\t\t\tTo know" << endl;
+	cout << "\t\t\t\tWhere" << endl;
+	cout << "\t\t\t\t\tThe pheasant" << endl;
+	cout << "\t\t\t\t\t\t\tIs" << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+	cout << " " << endl;
+
+	cout << "Zadanie 3:" << endl;
+	cout << "\t\t\t\t\tПродам Жигуль недорого" << endl;
+	cout << "\t\t\t\t\tТел.: 9 999 999 99 99" << endl;
+	cout << "\t\t\t\t\tПробег -1 км" << endl;
+	cout << "\t\t\t\t\tТолько вчера собрали на заводе" << endl;
+	cout << "\t\t\t\t\tНа нем ездил сам Ленин и не только" << endl;
+	cout << "\t\t\t\t\tВсего за 2 доширака";
+}
