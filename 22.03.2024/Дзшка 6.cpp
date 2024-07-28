03977056f953acda6c7d29b5392e71f3911262de
KirillFedotenko <159914000+KirillFedotenko@users.noreply.github.com>
2024-07-28 20:04:27 +0300

    Add files via upload

---
+++ 22.03.2024/Дзшка 6.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	// задание 1
+
+	/*
+	int num;
+	cout << "введите число: ";
+	cin >> num;
+	const int SIZE1 = 2;
+	const int SIZE2 = 5;
+	int array[SIZE1][SIZE2]{};
+	array[0][0] = num;
+	for (int i = 0; i < SIZE1; i++) {
+		for (int j = 0; j < SIZE2; j++) {
+			if (i == 0 && j < SIZE2) {
+				array[i][j+1] =  num *= 2;
+				cout << array[i][j] << " ";
+			}
+			else
+			{
+				array[i][j] = num *= 2;
+				cout << array[i][j] << " ";
+			}
+		}
+		cout << endl;
+	}
+	*/
+
+	// задание 2
+
+	/*
+	int num;
+	cout << "введите число: ";
+	cin >> num;
+	const int SIZE1 = 2;
+	const int SIZE2 = 5;
+	int array[SIZE1][SIZE2]{};
+	array[0][0] = num;
+	for (int i = 0; i < SIZE1; i++) {
+		for (int j = 0; j < SIZE2; j++) {
+				array[i][j] = num++;
+				cout << array[i][j] << " ";
+		}
+		cout << endl;
+	}
+	*/
+}
